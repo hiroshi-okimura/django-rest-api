44f469d2333e27e4520872ecf83d519dd660bb1a
o.hiroshi <o.hiroshi@zeta.inc>
2026-02-09 00:41:24 +0900

    簡単なTODO機能追加

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r"todos", views.TodoViewSet)
+
 urlpatterns = [
+    path("", include(router.urls)),
     path("test/", views.test_view, name="test"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,17 @@
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    TodoのCRUD操作を提供するViewSet
+    """
+
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
 
 
 @api_view(["GET"])
